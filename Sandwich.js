ef8f5dca6e76149bd7145b52e1beee0c5f85013b
Devendra <devendarkumarfea@gmail.com>
2022-08-19 21:32:27 +0530

    new files added

---
+++ Sandwich.js
@@ -0,0 +1,17 @@
+// function sandwich_cal (cheese, bread)
+// {
+//    var  stuff = parseInt(bread/2);
+function sandwich_cal (bread, cheese)
+{
+   var  stuff = parseInt(bread/2)
+   
+   if(stuff == cheese ){
+      console.log("No of sandwichs is ", stuff)
+   } else if(stuff > cheese){
+      console.log("No of sandwichs is ", cheese)
+   } else{
+      console.log("No of sandwichs is ", stuff)
+   }
+
+   
+}
